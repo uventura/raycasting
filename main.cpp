c0d34bb97e200988065780cf1fd1c171778b15e2
uventura <ualivent@gmail.com>
2021-08-21 20:24:26 -0300

    SetPoint Function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
+// compile: g++ main.cpp -o main -lGL -lglfw
+// execute: ./main
+
 #include <stdio.h>
 #include <GLFW/glfw3.h>
 
+#define WIDTH 600
+#define HEIGHT 400
+
+#define xpos(x) (2.0f/(float)WIDTH)*(float)x-1.0
+#define ypos(y) (2.0f/(float)HEIGHT)*(float)y-1.0
+
+void setPoint(float x, float y)
+{
+    // How much pixels a block requires
+    const int xpixel = 16;
+    const int ypixel = 16;
+
+    for(float i = xpixel*x; i < xpixel*(x+1); ++i)
+    {
+        glBegin(GL_POINTS);
+        for(float j = ypixel*y; j < ypixel*(y+1); ++j)
+            glVertex2f(xpos(i), ypos(j));
+        glEnd();
+    }    
+}
+
+// SCENARIO
 const int rows = 8;
 const int cols = 8;
 
@@ -45,17 +70,10 @@ int main()
         glClearColor(0.2f, 0.0f, 0.2f, 1.0f);
 
         glColor3f(1.0f, 1.0f, 0.0f);
-        //glPointSize(2.0/(float)cols);
-        //glPointSize(width/rows);
-
-        glPointSize(8.0f);
-        glBegin(GL_POINTS);
-        for(int i=0;i<8;++i)
-        {
-            glColor3f(1.0f, 1.0f-0.1f*i, 0.1*i);
-            glVertex2f(-0.5f+0.1*(i+1),(2.0/(float)width)*i);
-        }
-        glEnd();
+        setPoint(1,1);
+        glColor3f(1.0f,0.5f,1.0f);
+        setPoint(0,0);
+        setPoint(2,2);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
